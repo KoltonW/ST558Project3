ac6bd5ead3808374d7b2c023a579060b6b15b7d6
Kolton Wiebusch <krwiebusch@gmail.com>
2020-11-10 23:54:21 -0600

    Small update

--- Project3App/server.R
+++ Project3App/server.R
@@ -1,13 +1,22 @@
-
+#Read in packages
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(shinydashboard)
 
+#Bring in data and manipulate it
 cbb <- read_csv("cbb.csv")
+cbb %>% mutate(WinPct = round((W/G), 3)) -> cbb
+cbb %>% select(-TEAM, -CONF, -G, -W, -POSTSEASON, -SEED, -YEAR) -> cbb4Model
+cbb %>% select(-POSTSEASON, -SEED) -> cbb4Dat
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
 
+    #Create text info
+    output$inf <- renderText({
+        paste("test")
+    })
 
 
 
--- Project3App/ui.R
+++ Project3App/ui.R
@@ -1,21 +1,32 @@
-
+#Read in packages
 library(shiny)
 library(tidyverse)
 library(ggplot2)
 library(shinydashboard)
 
+#Bring in data and manipulate it
 cbb <- read_csv("cbb.csv")
+cbb %>% mutate(WinPct = round((W/G), 3)) -> cbb
+cbb %>% select(-POSTSEASON, -SEED) -> cbb4Dat
+cbb %>% select(-TEAM, -CONF, -G, -W, -POSTSEASON, -SEED, -YEAR) -> cbb4Model
 
+#Create App dashboard page
 dashboardPage(
     dashboardHeader(title = "App Dashboard"),
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Information", tabname = "info", icon = icon("info")),
-            menuItem("Data Exploration", tabname = "datinfo", icon = icon("book-open")),
-            menuItem("Clustering", tabname = "clust", icon = icon("ethernet")),
-            menuItem("Modeling", tabname = "modeling", icon = icon("brain")),
-            menuItem("Data Save", tabname = "save", icon = icon("folder"))
+            menuItem("Information", tabName = "info", icon = icon("info")),
+            menuItem("Data Exploration", tabName = "datinfo", icon = icon("book-open")),
+            menuItem("Clustering", tabName = "clust", icon = icon("ethernet")),
+            menuItem("Modeling", tabName = "modeling", icon = icon("brain")),
+            menuItem("Data Save", tabName = "save", icon = icon("folder"))
         )
     ),
-    dashboardBody()
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "info",
+                    box(title = "Information",  status = "primary", solidHeader = TRUE, 
+                        textOutput("inf")))
+        )
+    )
 )
