06eb1435a35a4d95cd28048f8ccbaa5157e45e14
Kolton Wiebusch <krwiebusch@gmail.com>
2020-11-11 22:51:28 -0600

    Completed Data Exploration tab

--- Project3App/server.R
+++ Project3App/server.R
@@ -1,14 +1,17 @@
 #Read in packages
 library(shiny)
 library(tidyverse)
+library(plotly)
 library(ggplot2)
 library(shinydashboard)
+library(DT)
 
 #Bring in data and manipulate it
 cbb <- read_csv("cbb.csv")
-cbb %>% mutate(WinPct = round((W/G), 3)) -> cbb
+cbb %>% mutate(WinPct = round((W/G), 3)) %>% rename(TwoPt_O = "2P_O", TwoPt_D = "2P_D", ThreePt_O = "3P_O", ThreePt_D = "3P_D") -> cbb
 cbb %>% select(-TEAM, -CONF, -G, -W, -POSTSEASON, -SEED, -YEAR) -> cbb4Model
 cbb %>% select(-POSTSEASON, -SEED) -> cbb4Dat
+cbb %>% select(-POSTSEASON, -SEED, -TEAM, -CONF, -YEAR) -> cbb4Dat2
 
 
 shinyServer(function(input, output) {
@@ -28,25 +31,66 @@ shinyServer(function(input, output) {
 "DRB: Offensive Rebound Rate Allowed", '<br/>', '<br/>',
 "FTR : Free Throw Rate (How often the given team shoots Free Throws)", '<br/>', '<br/>',
 "FTRD: Free Throw Rate Allowed", '<br/>', '<br/>',
-"2P_O: Two-Point Shooting Percentage", '<br/>', '<br/>',
-"2P_D: Two-Point Shooting Percentage Allowed", '<br/>', '<br/>',
-"3P_O: Three-Point Shooting Percentage", '<br/>', '<br/>',
-"3P_D: Three-Point Shooting Percentage Allowed", '<br/>', '<br/>',
+"TwoPt_O: Two-Point Shooting Percentage", '<br/>', '<br/>',
+"TwoPt_D: Two-Point Shooting Percentage Allowed", '<br/>', '<br/>',
+"ThreePt_O: Three-Point Shooting Percentage", '<br/>', '<br/>',
+"ThreePt_D: Three-Point Shooting Percentage Allowed", '<br/>', '<br/>',
 "ADJ_T: Adjusted Tempo (An estimate of the tempo (possessions per 40 minutes) a team would have against the team that wants to play at an average Division I tempo)", '<br/>', '<br/>',
 "WAB: Wins Above Bubble (The bubble refers to the cut off between making the NCAA March Madness Tournament and not making it)"))
     })
     
     output$purp <- renderUI({
         HTML(
-            paste0("The purpose of this app is to view what components of team play within college basketball led to success (in the form of a high winning percentage) or failure. Individual teams' statistical trends can be viewed over the course of five seasons, and they can be compared against another team as well. Supervised and unsupervised prediction models can be used to take a closer look at the response variable of WinPct (winning percentage). Plots viewed on the data exploration tab may be saved, as well as the dataset as a whole (or a subset of it) in the data save tab." , '<br/>',  '<br/>',
-                   "This information tab just gives an overview of the data and app. Clicking on the Data Exploration tab allows you to look at variables for different teams along with the data set as a data table. The Clustering tab allows the user to specify certain aspects of the clustering method and produces a dendogram that may be saved. The Modeling tab allows for two separate supervised learning models to predict for the response of winning percentage based on user-chosen settings. The Data Save tab allows the data set to be viewed, subsetted, and saved.")
+            paste0("The purpose of this app is to view what components of team play within college basketball led to success (in the form of a high winning percentage) or failure. Individual teams' statistical trends can be viewed over the course of five seasons, and they can be compared against the average values of each variable over that time span. Supervised and unsupervised prediction models can be used to take a closer look at the response variable of WinPct (winning percentage). Plots viewed on the data exploration tab may be saved, as well as the dataset as a whole (or a subset of it) in the data save tab." , '<br/>',  '<br/>',
+                   "This information tab just gives an overview of the data and app. Clicking on the Data Exploration tab allows you to look at variables for different teams along with their averages. The plot in this tab allows hovering and clicking on the graph as an additional functionality. The Clustering tab allows the user to specify certain aspects of the clustering method and produces a dendogram that may be saved. The Modeling tab allows for two separate supervised learning models to predict for the response of winning percentage based on user-chosen settings. The Data Save tab allows the data set to be viewed, subsetted, and saved.")
         )
     })
     
     output$src <- renderUI({
         h3("This data set comes from", a(href = "https://www.kaggle.com/andrewsundberg/college-basketball-dataset?select=cbb.csv", " the College Basketball Dataset on Kaggle."))
     })
+    
+    output$graphTitle <- renderUI({
+        text <- paste0(input$tm, " Trend Over 5 Year Span for the Variable ", input$stat)
+        h3(text)
+    })
+    
+    #Create team select input
+    output$team_selected <- renderUI({
+        teams_available <- cbb4Dat[cbb4Dat$CONF == input$conf, "TEAM"]
+        
+        selectInput("tm", strong("Select a Team"),
+                    choices = unique(teams_available),
+                    selected = '')
+    })
+    
+    #Team trend line plot
+    plotInput <- reactive({
+        g <- ggplot(cbb4Dat[cbb4Dat$TEAM == input$tm, ], aes(x = YEAR))
+        ggplotly(g + geom_line(aes_string(y = input$stat)) + geom_point(aes_string(y = input$stat)) +
+                     theme(axis.text.x =element_text(face="bold", color="black", size=12), 
+                           axis.text.y = element_text(face="bold", color="black", size=12)))
+    })
+    
+    output$teamPlot <- renderPlotly({
+        print(plotInput())
+    })
+    
+    #Create download button
+    output$dplot <- downloadHandler(
+        filename = function() { paste(input$tm, input$stat, '.png', sep='') },
+        content = function(file) {
+            ggsave(file, plot = plotInput(), device = "png")
+        })
+    
+    #Create summary
+    output$avgs <- renderText({
+        value <- as.name(input$stat)
+        paste0("The average value across all D1 college basketball for the selected variable of ", input$stat, " is: ", colMeans(cbb[, value]))
+    })
+    
 
-
+    
+    
 
 })
--- Project3App/ui.R
+++ Project3App/ui.R
@@ -1,13 +1,16 @@
 #Read in packages
 library(shiny)
 library(tidyverse)
-library(ggplot2)
+library(plotly)
 library(shinydashboard)
+library(ggplot2)
+library(DT)
 
 #Bring in data and manipulate it
 cbb <- read_csv("cbb.csv")
-cbb %>% mutate(WinPct = round((W/G), 3)) -> cbb
+cbb %>% mutate(WinPct = round((W/G), 3)) %>% rename(TwoPt_O = "2P_O", TwoPt_D = "2P_D", ThreePt_O = "3P_O", ThreePt_D = "3P_D") -> cbb
 cbb %>% select(-POSTSEASON, -SEED) -> cbb4Dat
+cbb %>% select(-POSTSEASON, -SEED, -TEAM, -CONF, -YEAR) -> cbb4Dat2
 cbb %>% select(-TEAM, -CONF, -G, -W, -POSTSEASON, -SEED, -YEAR) -> cbb4Model
 
 #Create App dashboard page
@@ -30,7 +33,34 @@ dashboardPage(
                     box(title = "Purpose", status = "primary", solidHeader = TRUE,
                         htmlOutput("purp")),
                     box(title = "Source", status = "primary", solidHeader = TRUE,
-                        uiOutput("src")))
+                        uiOutput("src"))),
+            
+            tabItem(tabName = "datinfo",
+                    box(title = "Inputs", status = "warning", solidHeader = TRUE, width = 4,
+                        h4("Choose options to find team and statistic:"),
+                        selectInput("conf", strong("Select a Conference"),
+                                    choices = c("Enter a conference..."="", sort(cbb4Dat$CONF)),
+                                    selected = ''),
+                        conditionalPanel(
+                            condition = "input.conf != '' ",
+                            uiOutput("team_selected")
+                        ),
+                        conditionalPanel(
+                            condition = "input.conf !== '' ",
+                            selectInput("stat", strong("Select a Statistic"),
+                                        choices = c("Choose a variable..."="", names(cbb4Dat2)),
+                                        selected = '')
+                        ),
+                        
+                        downloadButton("dplot", "Download this Plot")
+                        
+                        ),
+                    box(title = uiOutput("graphTitle"), status = "primary", solidHeader = TRUE,
+                        plotlyOutput("teamPlot"), width = 8),
+                    
+                    box(title = "Averages", status = "primary", solidHeader = TRUE, width = 12,
+                        textOutput("avgs"))
+                    )
         )
     )
 )
